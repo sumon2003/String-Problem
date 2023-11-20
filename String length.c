020c160df28cfb08603c2ec6efec3ea15711dc53
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-20 08:28:48 +0600

    length of a string

---
+++ String length.c
@@ -0,0 +1,23 @@
+/*
+Write a program in C to find the length of a string without using library functions.
+
+Test Data :
+Input the string : w3resource.com
+
+Expected Output :
+
+Length of the string is : 15 
+*/
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    fgets(str, sizeof str, stdin);
+    int i=0;
+    while(str[i] != '\0'){
+        i++;
+    }
+    printf("%d",i-1);
+    
+   return 0; 
+}
