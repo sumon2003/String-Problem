d6c43cc88638e408f55496a35b77a202ab043f06
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2023-11-20 18:40:19 +0600

    Word count from string

---
+++ String word count.c
@@ -0,0 +1,25 @@
+/*
+ Write a program in C to count the total number of words in a string.
+
+Test Data :
+Input the string : This is w3resource.com
+
+Expected Output :
+
+Total number of words in the string is : 3 
+*/
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    printf("Input the string : ");
+    fgets(str, sizeof str, stdin);
+    int i, count_word = 1;
+    while(str[i] != '\0'){
+        if(str[i]==' ' || str[i]=='\n' || str[i]=='\t'){
+            count_word++;
+        }
+        i++;
+    }
+    printf("Total number of words in the string is : %d",count_word-1);
+}    
