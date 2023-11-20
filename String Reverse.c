270c272ebfd98d6b7659fe38f2eaacab0128c39e
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-20 11:15:31 +0600

    String in Reverse

--- String Reverse.c
+++ String Reverse.c
@@ -1,3 +1,14 @@
+/*
+Write a program in C to print individual characters of a string in reverse order.
+
+Test Data :
+Input the string : w3resource.com
+
+Expected Output :
+
+The characters of the string in reverse are :
+m  o  c  .  e  c  r  u  o  s  e  r  3  w 
+*/
 #include<stdio.h>
 #include<string.h>
 int main(){
