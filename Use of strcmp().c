144a82f2d195795f05b0d6b89d268e3af11cb589
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2023-12-25 07:16:53 +0600

    strcmp() to compare

---
+++ Use of strcmp().c
@@ -0,0 +1,20 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+//*/   Md Sumon Hossain Khan   //*/
+#include <stdio.h>
+#include <string.h>
+int main() {
+   char str1[] = "kumon";
+   char str2[] = "shan";
+   int result = strcmp(str1, str2);
+   printf("%d\n",result);
+   if(result == 0){
+    printf("The two string are equal.");
+   }else if(result > 0){
+    printf("str1 is lexicografically greater than str2.");
+   }else{
+    printf("str1 is lexicografically less than str2.");
+   }
+
+    return 0;
+}
+
