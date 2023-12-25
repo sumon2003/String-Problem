f2ff2f4218493889e276d2004bd4f41d530c954c
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2023-12-25 08:04:30 +0600

    To reverse a string

---
+++ use of strrev().c
@@ -0,0 +1,17 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan   //*/
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+
+   char str[101] = "Md Sumon Hossain Khan";
+   
+   // Reverse the string using strrev (if available)
+   strrev(str);
+
+   printf("%s", str);
+
+   return 0;
+}
+
